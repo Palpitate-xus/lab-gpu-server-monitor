5a3090b4b73f2d2f229b9bcd7740c0d888e3e3ef
Palpitate-xus <palpitate.xus@outlook.com>
2026-08-26 11:13:48 +0000

    feat(frontend): dual-theme css variables + element-plus dark mode integration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,13 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 import './styles.css'
 import './cockpit.css'
+import './theme'
 
 const app = createApp(App)
 
